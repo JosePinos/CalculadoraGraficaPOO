68dad4665d29eaf7f40d48272cabdf6403082a70
Jose Pinos <jose.pinos@epn.edu.ec>
2023-01-21 09:05:46 -0500

    GUI agradable

--- calcu.java
+++ calcu.java
@@ -1,20 +1,24 @@
 import javax.swing.*;
 
 public class calcu {
-    private JButton a7Button;
+    private JTextField textField1;
+    private JButton a7Button1;
     private JButton a4Button;
-    private JButton a1Button1;
     private JButton a8Button;
     private JButton a5Button;
-    private JButton a2Button1;
-    private JButton a9Button;
-    private JButton a6Button;
-    private JButton a3Button;
-    private JButton a0Button;
-    private JTextField textField1;
-    private JButton button1;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton button7;
     private JButton xButton;
-    private JButton button3;
-    private JButton button4;
+    private JButton MODButton;
+    private JButton button10;
+    private JButton a0Button;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JButton button1;
     private JButton button2;
+    private JButton sqrtButton;
+    private JButton button4;
+    private JButton button3;
 }
