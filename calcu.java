8221d43b8931e655f3c91b24addba523b5e20117
Jose Pinos <jose.pinos@epn.edu.ec>
2023-01-21 09:35:41 -0500

    Listeners de los botones numéricos y configuracion ingreso datos(alinear)

--- calcu.java
+++ calcu.java
@@ -1,24 +1,101 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class calcu {
-    private JTextField textField1;
-    private JButton a7Button1;
-    private JButton a4Button;
-    private JButton a8Button;
-    private JButton a5Button;
-    private JButton a1Button;
-    private JButton a2Button;
-    private JButton button7;
-    private JButton xButton;
-    private JButton MODButton;
-    private JButton button10;
-    private JButton a0Button;
-    private JButton a3Button;
-    private JButton a6Button;
-    private JButton a9Button;
-    private JButton button1;
-    private JButton button2;
-    private JButton sqrtButton;
-    private JButton button4;
-    private JButton button3;
+    private JTextField pantalla;
+    private JButton Boton7;
+    private JButton Boton4;
+    private JButton Boton8;
+    private JButton Boton5;
+    private JButton Boton1;
+    private JButton Boton2;
+    private JButton BotonSumar;
+    private JButton BotonMultiplicar;
+    private JButton BotonModulo;
+    private JButton BotonPotencia;
+    private JButton Boton0;
+    private JButton Boton3;
+    private JButton Boton6;
+    private JButton Boton9;
+    private JButton BotonRestar;
+    private JButton BotonDividir;
+    private JButton BotonRaiz;
+    private JButton BotonPorcentaje;
+    private JButton RESPUESTA;
+    private JPanel Panel1;
+
+
+
+    public calcu() {
+        Boton0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"0");
+            }
+        });
+        Boton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"1");
+            }
+        });
+        Boton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"2");
+            }
+        });
+        Boton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"3");
+            }
+        });
+        Boton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"4");
+            }
+        });
+        Boton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"5");
+            }
+        });
+        Boton6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"6");
+            }
+        });
+        Boton7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"7");
+            }
+        });
+        Boton8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"8");
+            }
+        });
+        Boton9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pantalla.setText(pantalla.getText()+"9");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculadora");
+        frame.setContentPane( new calcu().Panel1 );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
--- mainn.java
+++
@@ -1,5 +0,0 @@
-public class mainn {
-    public static void main(String[] args) {
-
-    }
-}
