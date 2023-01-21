39886f08c2f8a0e8ddf6ec7082b378e1de78a5ed
Jose Pinos <jose.pinos@epn.edu.ec>
2023-01-21 08:31:41 -0500

    Interfaz fea V1.0

--- calcu.java
+++ calcu.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class calcu {
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a1Button1;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton a2Button1;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton a0Button;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton xButton;
+    private JButton button3;
+    private JButton button4;
+    private JButton button2;
 }
